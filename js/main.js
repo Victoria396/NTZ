880ddccf7996e75649987e0451ae2603c259f520
Victoria396 <95283353+Victoria396@users.noreply.github.com>
2022-09-14 13:46:40 +0700

    сайт почти готов, осталось разобраться со стрелочками в открывающемся меню

--- js/main.js
+++ js/main.js
@@ -85,3 +85,38 @@ document.querySelector(".specialist__prev").addEventListener('click', function()
         specLine.style.left = -offset2 + 'px';
     }
 });
+
+// код для скролла
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offsetAnim(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                animItem.classList.remove('._active');
+            }
+        }
+    }
+    function offsetAnim(el) {
+    const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+}
